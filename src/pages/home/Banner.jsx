cbf22ca19b106b68b1b40af9267886cc0e49048c
Salvador perez <salverez90@gmail.com>
2024-06-03 00:01:18 +0600

    banner

--- src/pages/home/Banner.jsx
+++ src/pages/home/Banner.jsx
@@ -1,9 +1,14 @@
+import banner from "../../assets/employee.jpg"
 
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div className=" text-white py-32 bg-cover bg-top" style={{ backgroundImage: `url(${banner})`, }}>
+            <div className="bg-black bg-opacity-40 w-2/3 mx-auto text-center p-4 space-y-4">
+                <h1 className=" text-3xl font-bold">Leading the Way in Digital Talent Solutions</h1>
+                <p className=" text-lg ">Connecting Talent with Opportunities Seamlessly – Experience Excellence with Talent Syncro</p>
+                <button className=" border-2 border-primary px-2 rounded-lg">Join Us</button>
+            </div>
         </div>
     );
 };
