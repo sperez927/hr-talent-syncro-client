1d30b263fa933828e5ea05b17e86830603143726
Salvador perez <salverez90@gmail.com>
2024-06-03 15:53:00 +0600

    social login

--- src/pages/loginRegister/Register.jsx
+++ src/pages/loginRegister/Register.jsx
@@ -53,7 +53,6 @@ const Register = () => {
                 const newUser = {
                     name,
                     email,
-                    password,
                     photoUrl,
                     role: data.role,
                     bank_account_no: data.bank_account_no,
--- src/pages/loginRegister/SocialLogin.jsx
+++ src/pages/loginRegister/SocialLogin.jsx
@@ -5,6 +5,8 @@ import 'react-toastify/dist/ReactToastify.css';
 import { useLocation, useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub } from "react-icons/fa";
+import axios from "axios";
+import { BASE_URL } from "../../constent/constent";
 
 const SocialLogin = () => {
     const { googleSignIn, githubSignIn } = useContext(AuthContext);
@@ -15,6 +17,25 @@ const SocialLogin = () => {
         googleSignIn()
             .then(result => {
                 console.log(result);
+
+                const newUser = {
+                    name: result.user.displayName,
+                    email: result.user.email,
+                    photoUrl: result.user.photoURL,
+                    role: 'Employee',
+                    bank_account_no: null,
+                    salary: null,
+                    designation: null,
+                };
+
+                console.log("New User Data:", newUser);
+
+                axios.post(BASE_URL + '/user', newUser, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+
                 toast.success('Successfully Logged In');
                 navigate(location?.state ? location.state : "/");
             })
@@ -28,6 +49,25 @@ const SocialLogin = () => {
         githubSignIn()
             .then(result => {
                 console.log(result);
+
+                const newUser = {
+                    name: result.user.displayName,
+                    email: result.user.email,
+                    photoUrl: result.user.photoURL,
+                    role: 'Employee',
+                    bank_account_no: null,
+                    salary: null,
+                    designation: null,
+                };
+
+                console.log("New User Data:", newUser);
+
+                axios.post(BASE_URL + '/user', newUser, {
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+
                 toast.success('Successfully Logged In');
                 navigate(location?.state ? location.state : "/");
             })
